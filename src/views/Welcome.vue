6f6fa79e31fbc5ef41dce9a6045ac1aa4f1c951d
Sebastian Otto <44194014+Bineryss@users.noreply.github.com>
2022-01-27 21:14:10 +0100

    init

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome container">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from '@vue/runtime-core'
+
+export default defineComponent({})
+</script>
+
+<style scoped>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
